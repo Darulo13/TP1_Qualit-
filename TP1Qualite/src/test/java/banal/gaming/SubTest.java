3b07c8635cee40462367e557bd9072e5dace1b6a
Dresseur_Panda <mathias.garaios@gmail.com>
2025-09-08 11:59:08 +0200

    Modification de SubTest pour faire fonctionner le test et ajout de la javadoc dans les 2 classes

--- TP1Qualite/src/test/java/banal/gaming/SubTest.java
+++ TP1Qualite/src/test/java/banal/gaming/SubTest.java
@@ -1,12 +1,13 @@
 package banal.gaming;
 
+
 import junit.framework.TestCase;
 
-public class SubTest {
+public class SubTest extends TestCase{
 
     public void testSub(){
         int a = 3;
         int b = 2;
-        TestCase.assertEquals(1, new Sub().sub(a, b)); 
+        assertEquals(1, new Sub().sub(a, b)); 
     }
 }
