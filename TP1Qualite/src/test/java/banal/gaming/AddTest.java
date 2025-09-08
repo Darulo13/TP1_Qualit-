48cd6da795111d194c86eeca926205a9c411282a
Rudy <rudyciardi@gmail.com>
2025-09-08 11:57:31 +0200

    Modification du test unitaire

--- TP1Qualite/src/test/java/banal/gaming/AddTest.java
+++ TP1Qualite/src/test/java/banal/gaming/AddTest.java
@@ -3,13 +3,13 @@ package banal.gaming;
 
 import junit.framework.TestCase;
 
-public class AddTest {
-    public void testAdd() {
+public class AddTest extends TestCase {
+    public void testAdd(){
         int a = 2;
         int b = 3;
         Add adder = new Add();
         int result = adder.add(a, b);
-        TestCase.assertEquals(5, result);
+        assertEquals(5, result);
     }
 
 }
